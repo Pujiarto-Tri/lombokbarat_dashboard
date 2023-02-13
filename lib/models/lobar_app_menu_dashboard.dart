b57a33bec5b3ffa0341779dfbdbdc990945b8b9a
Pujiarto-Tri <pujiarto.ta@gmail.com>
2023-02-13 11:25:48 +0800

    modified:   lib/models/lobar_app_menu_dashboard.dart 	modified:   pubspec.yaml

--- lib/models/lobar_app_menu_dashboard.dart
+++ lib/models/lobar_app_menu_dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:ppid_flutter/screens/screen.dart';
+import 'package:flutter/material.dart';
 
 final List<Map<String, dynamic>> appMenu = [
   {
@@ -22,19 +23,19 @@ final List<Map<String, dynamic>> appMenu = [
     'app_name': 'JDIH',
     'routeName': WebViewScreen.routeName,
     'arguments': {'link': 'https://jdih.lombokbaratkab.go.id/'},
-    'icon': 'assets/images/app_icon/logo-LPSE-3.png'
+    'icon': 'assets/images/app_icon/logo_jdih.png'
   },
   {
     'app_name': 'SAKIP',
     'routeName': WebViewScreen.routeName,
     'arguments': {'link': 'http://sakip.lombokbaratkab.go.id/portal/home'},
-    'icon': 'assets/images/app_icon/logo-LPSE-3.png'
+    'icon': 'assets/images/app_icon/logo_sakip.png',
   },
   {
     'app_name': 'Survey',
     'routeName': WebViewScreen.routeName,
     'arguments': {'link': 'https://ikm.lombokbaratkab.go.id/'},
-    'icon': 'assets/images/app_icon/logo-LPSE-3.png'
+    'icon': 'assets/images/app_icon/logo_survey.png'
   },
   {
     'app_name': 'RPJMD',
@@ -43,17 +44,17 @@ final List<Map<String, dynamic>> appMenu = [
       'link':
           'https://lombokbaratkab.go.id/rencana-pembangunan-jangka-menengah-daerah-rpjmd-kabupaten-lombok-barat-tahun-2019-2024/'
     },
-    'icon': 'assets/images/app_icon/logo-LPSE-3.png'
+    'icon': 'assets/images/app_icon/logo_rpjmd.png'
   },
   {
     'app_name': 'E-Kinerja',
     'routeName': WebViewScreen.routeName,
     'arguments': {'link': 'https://ekinerja-asn.lombokbaratkab.go.id/'},
-    'icon': 'assets/images/app_icon/logo-LPSE-3.png'
+    'icon': 'assets/images/app_icon/logo_ekinerja.png'
   },
   {
     'app_name': 'OPD',
     'routeName': OpdListScreen.routeName,
-    'icon': 'assets/images/app_icon/logo-LPSE-3.png'
+    'icon': 'assets/images/app_icon/logo_opd.png'
   },
 ];
