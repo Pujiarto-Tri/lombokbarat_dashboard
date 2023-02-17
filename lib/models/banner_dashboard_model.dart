944cf96e82ac6c05823c3a9ba0265697ce668f97
ajied <pujiarto.ta@gmail.com>
2023-02-17 10:28:53 +0800

    modified:   lib/screens/lobar_dashboard/dashboard.dart 	modified:   pubspec.yaml

---
+++ lib/models/banner_dashboard_model.dart
@@ -0,0 +1,31 @@
+import 'package:ppid_flutter/screens/screen.dart';
+
+final List<Map<String, dynamic>> bannerDashboard = [
+  {
+    'banner_title': 'Lapor!',
+    'routeName': WebViewScreen.routeName,
+    'img': 'assets/images/dashboard_banner/lapor_banner.png',
+    'arguments': {'link': 'https://lombokbarat.lapor.go.id/'}
+  },
+  {
+    'banner_title': 'Pajak',
+    'routeName': WebViewScreen.routeName,
+    'img': 'assets/images/dashboard_banner/pajak_banner.jpeg',
+    'arguments': {
+      'link':
+          'https://bapenda.lombokbaratkab.go.id/pengumuman/pembayaran-pbb-p2-sekarang-sudah-on-line--29/'
+    }
+  },
+  {
+    'banner_title': 'g20',
+    'routeName': WebViewScreen.routeName,
+    'img': 'assets/images/dashboard_banner/g20_banner.jpeg',
+    'arguments': {'link': 'https://lombokbaratkab.go.id/capaian-kinerja-2022/'}
+  },
+  {
+    'banner_title': 'g20',
+    'routeName': WebViewScreen.routeName,
+    'img': 'assets/images/dashboard_banner/g20_banner.jpeg',
+    'arguments': {'link': 'https://lombokbaratkab.go.id/capaian-kinerja-2022/'}
+  },
+];
--- lib/screens/lobar_dashboard/dashboard.dart
+++ lib/screens/lobar_dashboard/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ppid_flutter/models/lobar_app_menu_dashboard.dart';
 import 'package:ppid_flutter/models/news_model.dart';
 import 'package:ppid_flutter/models/agenda_model.dart';
+import 'package:ppid_flutter/models/banner_dashboard_model.dart';
 import 'package:ppid_flutter/screens/screen.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:http/http.dart' as http;
@@ -113,9 +114,14 @@ class DashboardScreenState extends State<DashboardScreen> {
                       height: 20,
                     ),
                     const NewsDashboard(),
-                    const Divider(),
-                    const MenuDashboard(),
-                    const Divider(),
+                    const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Card(elevation: 3, child: MenuDashboard()),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const BannerDashboard(),
                     const SizedBox(
                       height: 10,
                     ),
@@ -643,6 +649,54 @@ class _AgendaBupatiDashboardState extends State<AgendaBupatiDashboard> {
             )
         ],
       ),
+    );
+  }
+}
+
+class BannerDashboard extends StatelessWidget {
+  const BannerDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 120,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: bannerDashboard.length,
+            itemBuilder: (context, index) {
+              final banner = bannerDashboard[index];
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, banner['routeName'],
+                        arguments: banner['arguments']);
+                  },
+                  child: Container(
+                    height: 120,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage(banner['img']),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: const Text(''),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
