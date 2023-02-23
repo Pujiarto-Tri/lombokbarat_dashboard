e1b8835b042692e90e7445b7b621e57cde43ffb1
Pujiarto-Tri <pujiarto.ta@gmail.com>
2023-02-23 15:38:27 +0800

    modified:   lib/screens/webview_screen/srikandi_webview_screen.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/webview_screen/srikandi_webview_screen.dart
+++ lib/screens/webview_screen/srikandi_webview_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+// InAppWebViewSettings useHybrid =
+//   //     InAppWebViewSettings(useHybridComposition: true);
 class SrikandiWebViewScreen extends StatefulWidget {
   const SrikandiWebViewScreen({Key? key}) : super(key: key);
 
@@ -12,31 +16,60 @@ class SrikandiWebViewScreen extends StatefulWidget {
 }
 
 class _SrikandiWebViewScreenState extends State<SrikandiWebViewScreen> {
-  InAppWebViewController? _controller;
+  final GlobalKey srikandiWebViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+  InAppWebViewSettings settings = InAppWebViewSettings();
+  PullToRefreshController? pullToRefreshController;
+  PullToRefreshSettings pullToRefreshSettings = PullToRefreshSettings(
+    color: Colors.blue,
+  );
+  bool pullToRefreshEnabled = true;
   bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    settings.userAgent =
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+
+    pullToRefreshController = kIsWeb
+        ? null
+        : PullToRefreshController(
+            settings: pullToRefreshSettings,
+            onRefresh: () async {
+              if (defaultTargetPlatform == TargetPlatform.android) {
+                webViewController?.reload();
+              } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+                webViewController?.loadUrl(
+                    urlRequest:
+                        URLRequest(url: await webViewController?.getUrl()));
+              }
+            },
+          );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Srikandi Web View'),
+        title: const Text('Srikandi'),
       ),
-      body: RefreshIndicator(
-        onRefresh: _refreshWebView,
-        child: Stack(
-          children: [
-            InAppWebView(
-              initialUrlRequest: URLRequest(
-                url: Uri.parse('https://srikandi.arsip.go.id'),
-              ),
-              initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(
-                  userAgent:
-                      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
-                ),
-              ),
-              onWebViewCreated: (controller) {
-                _controller = controller;
+      body: Column(
+        children: <Widget>[
+          if (_isLoading)
+            LinearProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+            ),
+          Expanded(
+            key: srikandiWebViewKey,
+            child: InAppWebView(
+              initialUrlRequest:
+                  URLRequest(url: WebUri("https://srikandi.arsip.go.id/")),
+              initialSettings: settings,
+              pullToRefreshController: pullToRefreshController,
+              onWebViewCreated: (InAppWebViewController controller) {
+                webViewController = controller;
               },
               onLoadStart: (controller, url) {
                 setState(() {
@@ -47,27 +80,30 @@ class _SrikandiWebViewScreenState extends State<SrikandiWebViewScreen> {
                 setState(() {
                   _isLoading = false;
                 });
+                pullToRefreshController?.endRefreshing();
+              },
+              onReceivedError: (controller, request, error) {
+                setState(() {
+                  _isLoading = false;
+                });
+                pullToRefreshController?.endRefreshing();
+              },
+              onProgressChanged: (controller, progress) {
+                if (progress == 100) {
+                  setState(() {
+                    _isLoading = false;
+                  });
+                  pullToRefreshController?.endRefreshing();
+                } else {
+                  setState(() {
+                    _isLoading = true;
+                  });
+                }
               },
             ),
-            if (_isLoading)
-              const Center(
-                child: CircularProgressIndicator(),
-              ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
-  }
-
-  Future<void> _refreshWebView() async {
-    if (_controller != null) {
-      setState(() {
-        _isLoading = true;
-      });
-      await _controller!.reload();
-      setState(() {
-        _isLoading = false;
-      });
-    }
   }
 }
